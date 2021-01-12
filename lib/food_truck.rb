69a92280e3ef9d278183a68275959fa38822a89e
Jenny Branham <branhamjenny@gmail.com>
2021-01-12 14:16:48 -0600

    Add check_stock and stock

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -4,6 +4,18 @@ class FoodTruck
 
   def initialize(name)
     @name = name
-    @inventory = []
+    @inventory = Hash.new
+  end
+
+  def check_stock(item)
+    if @inventory[item]
+      @inventory[item]
+    else
+      0
+    end
+  end
+
+  def stock(item, amount)
+    @inventory[item] = amount
   end
 end
--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -1,6 +1,7 @@
 require 'minitest/pride'
 require 'minitest/autorun'
 require './lib/food_truck'
+require './lib/item'
 
 class FoodTruckTest < MiniTest::Test
   def test_it_exists
@@ -12,6 +13,24 @@ class FoodTruckTest < MiniTest::Test
     food_truck = FoodTruck.new("Rocky Mountain Pies")
 
     assert_equal 'Rocky Mountain Pies', food_truck.name
-    assert_equal [], food_truck.inventory
+    assert_equal Hash.new, food_truck.inventory
+  end
+
+  def test_check_stock
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+    item = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    assert_equal 0, food_truck.check_stock(item)
+
+    food_truck.stock(item, 30)
+    assert_equal 30, food_truck.check_stock(item)
+  end
+
+  def test_stock
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+    item = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+
+    food_truck.stock(item, 30)
+    hash = { item => 30 }
+    assert_equal hash, food_truck.inventory
   end
 end
